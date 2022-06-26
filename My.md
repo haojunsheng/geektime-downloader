72be73f2762d91872b51e8eb930c7caadedd93ed
haojunsheng <1324722673@qq.com>
2022-06-26 23:05:17 +0800

    markdown图片链接保留

---
+++ My.md
@@ -0,0 +1,9 @@
+获取所有课程
+
+geektime-dl login --phone xxxxxx --password xxxxxx
+
+./geektime-dl column|awk '{print $4}'|grep -v ID
+
+在使用geektime-downloader下载课程
+
+
--- internal/markdown/markdown.go
+++ internal/markdown/markdown.go
@@ -2,21 +2,14 @@ package markdown
 
 import (
 	"context"
+	md "github.com/JohannesKaufmann/html-to-markdown"
+	"github.com/nicoxiang/geektime-downloader/internal/pkg/filenamify"
+	"golang.org/x/sync/errgroup"
 	"os"
 	"path"
-	"path/filepath"
 	"regexp"
-	"strconv"
 	"strings"
 	"sync"
-	"time"
-
-	md "github.com/JohannesKaufmann/html-to-markdown"
-	"github.com/go-resty/resty/v2"
-	"github.com/nicoxiang/geektime-downloader/internal/pkg/filenamify"
-	pgt "github.com/nicoxiang/geektime-downloader/internal/pkg/geektime"
-	"github.com/nicoxiang/geektime-downloader/internal/pkg/logger"
-	"golang.org/x/sync/errgroup"
 )
 
 var (
@@ -52,31 +45,31 @@ func Download(ctx context.Context, html, title, dir string, aid, concurrency int
 	}
 	// step2: download images
 	var ss = &markdownString{s: markdown}
-	imageURLs := findAllImages(markdown)
+	//imageURLs := findAllImages(markdown)
+	//
+	//// images/aid/imageName.png
+	//imagesFolder := filepath.Join(dir, "images", strconv.Itoa(aid))
 
-	// images/aid/imageName.png
-	imagesFolder := filepath.Join(dir, "images", strconv.Itoa(aid))
-
-	c := resty.New()
-	c.SetOutputDirectory(imagesFolder).
-		SetRetryCount(1).
-		SetTimeout(5*time.Second).
-		SetHeader(pgt.UserAgentHeaderName, pgt.UserAgentHeaderValue).
-		SetHeader(pgt.OriginHeaderName, pgt.GeekBang).
-		SetLogger(logger.DiscardLogger{})
+	//c := resty.New()
+	//c.SetOutputDirectory(imagesFolder).
+	//	SetRetryCount(1).
+	//	SetTimeout(5*time.Second).
+	//	SetHeader(pgt.UserAgentHeaderName, pgt.UserAgentHeaderValue).
+	//	SetHeader(pgt.OriginHeaderName, pgt.GeekBang).
+	//	SetLogger(logger.DiscardLogger{})
 
 	g := new(errgroup.Group)
 	ch := make(chan string, concurrency)
 
-	for i := 0; i < concurrency; i++ {
-		g.Go(func() error {
-			return writeImageFile(ctx, ch, dir, imagesFolder, c, ss)
-		})
-	}
-
-	for _, imageURL := range imageURLs {
-		ch <- imageURL
-	}
+	//for i := 0; i < concurrency; i++ {
+	//	g.Go(func() error {
+	//		return writeImageFile(ctx, ch, dir, imagesFolder, c, ss)
+	//	})
+	//}
+	//
+	//for _, imageURL := range imageURLs {
+	//	ch <- imageURL
+	//}
 	close(ch)
 	err = g.Wait()
 	if err != nil {
@@ -113,49 +106,4 @@ func getDefaultConverter() *md.Converter {
 		converter = md.NewConverter("", true, nil)
 	}
 	return converter
-}
-
-func writeImageFile(ctx context.Context, imageURLs chan string, dir, imagesFolder string, c *resty.Client, ms *markdownString) (err error) {
-	var es []error
-loop:
-	for {
-		select {
-		case <-ctx.Done():
-			for range imageURLs {
-			}
-		case imageURL, ok := <-imageURLs:
-			if !ok {
-				break loop
-			}
-			if imageURL == "" {
-				return
-			}
-			segments := strings.Split(imageURL, "/")
-			f := segments[len(segments)-1]
-			if i := strings.Index(f, "?"); i > 0 {
-				f = f[:i]
-			}
-			imageLocalFullPath := filepath.Join(imagesFolder, f)
-			rel, err := filepath.Rel(dir, imageLocalFullPath)
-			if err != nil {
-				es = append(es, err)
-				break loop
-			}
-
-			_, err = c.R().
-				SetContext(ctx).
-				SetOutput(f).
-				Get(imageURL)
-			if err != nil {
-				es = append(es, err)
-				continue
-			}
-
-			ms.ReplaceAll(imageURL, filepath.ToSlash(rel))
-		}
-	}
-	if len(es) > 0 {
-		return es[0]
-	}
-	return nil
 }
