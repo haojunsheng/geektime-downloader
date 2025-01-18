b218984b9c9b97eb2e65289318938e1074c48204
haojunsheng <1324722673@qq.com>
2025-01-18 13:29:53 +0800

    图片不下载

--- internal/markdown/markdown.go
+++ internal/markdown/markdown.go
@@ -2,13 +2,11 @@ package markdown
 
 import (
 	"context"
-	"errors"
 	"net/url"
 	"os"
 	"path"
 	"path/filepath"
 	"regexp"
-	"strconv"
 	"strings"
 	"sync"
 
@@ -59,27 +57,27 @@ func Download(ctx context.Context, html, title, dir string, aid int, overwrite b
 	// step2: download images
 	var ss = &markdownString{s: markdown}
 	//imageURLs := findAllImages(markdown)
-
-	// images/aid/imageName.png
+	//
+	//// images/aid/imageName.png
 	//imagesFolder := filepath.Join(dir, "images", strconv.Itoa(aid))
-
+	//
 	//if _, err := os.Stat(imagesFolder); errors.Is(err, os.ErrNotExist) {
 	//	os.MkdirAll(imagesFolder, os.ModePerm)
 	//}
-
+	//
 	//err = writeImageFile(ctx, imageURLs, dir, imagesFolder, ss)
-
+	//
 	//if err != nil {
 	//	return false, err
 	//}
 
-	//f, err := os.Create(fullName)
-	//defer func() {
-	//	_ = f.Close()
-	//}()
-	//if err != nil {
-	//	return false, err
-	//}
+	f, err := os.Create(fullName)
+	defer func() {
+		_ = f.Close()
+	}()
+	if err != nil {
+		return false, err
+	}
 	// step3: write md file
 	_, err = f.WriteString("# " + title + "\n" + ss.s)
 	if err != nil {
@@ -107,4 +105,55 @@ func getDefaultConverter() *md.Converter {
 		converter = md.NewConverter("", true, nil)
 	}
 	return converter
+}
+
+func writeImageFile(ctx context.Context,
+	imageURLs []string,
+	dir,
+	imagesFolder string,
+	ms *markdownString,
+) (err error) {
+	for _, imageURL := range imageURLs {
+		segments := strings.Split(imageURL, "/")
+		f := segments[len(segments)-1]
+		if i := strings.Index(f, "?"); i > 0 {
+			f = f[:i]
+		}
+		imageLocalFullPath := filepath.Join(imagesFolder, f)
+
+		headers := make(map[string]string, 2)
+		headers[geektime.Origin] = geektime.DefaultBaseURL
+		headers[geektime.UserAgent] = geektime.DefaultUserAgent
+
+		_, err := downloader.DownloadFileConcurrently(ctx, imageLocalFullPath, imageURL, headers, 1)
+
+		if err != nil {
+			return err
+		}
+
+		rel, _ := filepath.Rel(dir, imageLocalFullPath)
+		ms.ReplaceAll(imageURL, filepath.ToSlash(rel))
+	}
+	return nil
+}
+
+func isImageURL(urlStr string) (bool, error) {
+	// 解析 URL
+	parsedURL, err := url.ParseRequestURI(urlStr)
+	if err != nil {
+		return false, err
+	}
+	// 提取路径部分
+	filePath := parsedURL.Path
+
+	// 获取文件扩展名并转换为小写
+	ext := strings.ToLower(path.Ext(filePath))
+
+	// 检查扩展名是否属于图片类型
+	switch ext {
+	case ".jpg", ".jpeg", ".png", ".gif", ".webp", ".bmp", ".tiff":
+		return true, nil
+	default:
+		return false, nil
+	}
 }
